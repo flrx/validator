ced399b85d1542d0254f6a3db8ea0523b7c6d1b4
Ibrahim Mubarak <ibrahim@faimtech.in>
2019-10-21 15:17:00 +0530

    Add a rule to run all rules
    Took 1 hour 7 minutes . Gitlab /spend 1h 7m

---
+++ lib/src/rules/all_rule.dart
@@ -0,0 +1,34 @@
+import 'package:flrx_validator/src/rules/rule.dart';
+
+/// A [Rule] subclass which runs validation on the list of [Rule]
+/// and does not stop if any of them fail.
+class AllRule<T> extends Rule<T> {
+  /// The list of [Rule] to be validated for.
+  final List<Rule<T>> _ruleList;
+
+  final String Function(List<String>) concatenator;
+
+  /// The default constructor
+  AllRule(
+    this._ruleList, {
+    String validationMessage,
+    this.concatenator = joinWithNewLine,
+  }) : super(validationMessage);
+
+  static String joinWithNewLine(List<String> validationMessages) =>
+      validationMessages.join("\n");
+
+  @override
+  String onValidate(String entityName, T value) {
+    List<String> allRulesValidationMessage = _ruleList
+        .map((Rule<T> rule) => rule.validate(entityName, value))
+        .where((String validationMessage) => validationMessage != null)
+        .toList();
+
+    if (allRulesValidationMessage.isEmpty) {
+      return null;
+    }
+
+    return concatenator(allRulesValidationMessage);
+  }
+}
---
+++ test/unit/rules/all_rule_test.dart
@@ -0,0 +1,38 @@
+import 'package:flrx_validator/src/rules/all_rule.dart';
+import 'package:flrx_validator/src/utils/string_utils.dart';
+import 'package:flrx_validator/validator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group("AllRule", () {
+    Rule<String> rule = AllRule<String>(
+      <Rule<String>>[
+        MinLengthRule(6),
+        RegexRule(
+          r'(?=.*[a-z])',
+          validationMessage: "It should contain one lowercase character",
+        ),
+      ]
+          .map((Rule<String> rule) =>
+              rule..transformMessage = StringUtils.replaceWithValues)
+          .toList(),
+    );
+    rule.transformMessage = StringUtils.replaceWithValues;
+
+    test('Runs all rules', () {
+      String message = rule.validate("Password", "123");
+      expect(message,
+          "Password should be more than 6 characters\nIt should contain one lowercase character");
+    });
+
+    test('Validated message exists only for rule that fails', () {
+      String message = rule.validate("Password", "abcd");
+      expect(message, "Password should be more than 6 characters");
+    });
+
+    test('Validated message is null when all rules pass', () {
+      String message = rule.validate("Password", "1234abcd");
+      expect(message, null);
+    });
+  });
+}
